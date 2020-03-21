3c5d6cb5c85e339733076cce4371980145a2c1fd
rsk-robin <aasrobinen6989@mail.ru>
2020-03-21 15:13:33 +0000

    comlete 3 task

--- lab2/src/revert_string/dinamiclib.sh
+++ lab2/src/revert_string/dinamiclib.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-gcc -fPIC -c revert_string.c -o revert_string.o
-gcc -shared revert_string.o -o librevert.so
+gcc -o librevert.so -shared -fPIC revert_string.c
+export LD_LIBRARY_PATH=$(pwd)
 gcc main.c -L. -lrevert -o dinamic_reverse
-export LD_LIBRARY_PATH=$(/projects/os_lab_2019/lab2/src/revert_string)
-rm *.o
