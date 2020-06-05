a024569cade2238e16f1536536c428e1a7d6cd29
rsk-robin <aasrobinen6989@mail.ru>
2020-06-05 20:42:14 +0000

    fix task3

--- lab5/src/task3.c
+++ lab5/src/task3.c
@@ -8,16 +8,16 @@ pthread_mutex_t lock1, lock2;
 void *resource1(){
     pthread_mutex_lock(&lock1);
 
-    printf("Job started in resource1..\n");
+    printf("Работа началась в resource1..\n");
     sleep(2);
 
-    printf("Trying to get resource2\n");
+    printf("Пробовать получить resource2\n");
 
     pthread_mutex_lock(&lock2); 
-    printf("Aquired resource2\n");
+    printf("Приобретенный resource2\n");
     pthread_mutex_unlock(&lock2);
 
-    printf("Job finished in resource1..\n");
+    printf("Работа закончена в resource1..\n");
 
     pthread_mutex_unlock(&lock1);
 
@@ -27,16 +27,16 @@ void *resource1(){
 void *resource2(){
     pthread_mutex_lock(&lock2);
 
-    printf("Job started in resource2..\n");
+    printf("Работа началась в resource2..\n");
     sleep(2);
 
-    printf("Trying to get resource1\n");
+    printf("Пробовать получить resource1\n");
     
     pthread_mutex_lock(&lock1); 
-    printf("Aquired resource1\n");
+    printf("Приобретенный resource1\n");
     pthread_mutex_unlock(&lock1);
 
-    printf("Job finished in resource2..\n");
+    printf("Работа закончена в resource2..\n");
 
     pthread_mutex_unlock(&lock2);
 
